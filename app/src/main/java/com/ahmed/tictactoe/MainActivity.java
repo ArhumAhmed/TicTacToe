ff253c2a7de9a2374e0293d9e5defdad4303b27b
arhum <arhumahmed96@gmail.com>
2019-04-03 19:56:41 -0400

    Made a setup. Experimenting with how I want each sector to be able to respond on a click. Trying to have invisible buttons in each location.

--- app/src/main/java/com/ahmed/tictactoe/MainActivity.java
+++ app/src/main/java/com/ahmed/tictactoe/MainActivity.java
@@ -2,12 +2,21 @@ package com.ahmed.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    char arr[] = new char[9]; //Array to store the locations of the boxes (3x3)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void userMove(View v)
+    {
+
+
+
     }
 }
